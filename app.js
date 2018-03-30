f2181940a3104f6d92716c5e1d4bb4afbf30c750
katcozadd <krosecozadd@gmail.com>
2018-03-30 13:16:54 -0600

    Created basic functionality of list

---
+++ app.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+
+	    $('#todo').on('submit', function(event) {
+	    event.preventDefault();
+    	var addOn = $('input[name=textBox]').val();
+    	$('ul').append('<li>' + addOn + '</li>');
+    	
+    });
+});
